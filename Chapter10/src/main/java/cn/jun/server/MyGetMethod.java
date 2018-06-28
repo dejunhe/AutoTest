eeda960386e2df0724dd675cebf001db493d90fd
879554326 <879554326@qq.com>
2018-06-28 19:01:33 +0800

    提交一点

--- Chapter10/src/main/java/cn/jun/server/MyGetMethod.java
+++ Chapter10/src/main/java/cn/jun/server/MyGetMethod.java
@@ -2,20 +2,61 @@ package cn.jun.server;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class MyGetMethod {
+    /**
+     * 开发一个需要携带参数才能访问的get请求。
+     * 第一种实现方式：usl:key=value&key=value
+     * 我们来模拟获取商品列表
+     *
+     */
+    @RequestMapping(value = "/get/with/param", method = RequestMethod.GET)
+    public Map<String,Integer> getList(@RequestParam Integer start,
+                                       @RequestParam Integer end){
+        Map<String,Integer> myList = new HashMap<>();
+        myList.put("鞋",400);
+        myList.put("干脆面",1);
+        myList.put("衬衫",300);
+        return myList;
+    }
 
-    @RequestMapping(value = "/getCookies",method = RequestMethod.GET)
-    public String getCookies(HttpServletResponse response){
+
+
+    @RequestMapping(value = "/getCookies", method = RequestMethod.GET)
+    public String getCookies(HttpServletResponse response) {
         //HttpServletRequest 装请求信息的类
         //HttpServletResponse 装响应信息的类
-        Cookie cookie = new Cookie("login","true");
+        Cookie cookie = new Cookie("login", "true");
         response.addCookie(cookie);
         return "恭喜你获得cookies成功!";
     }
+
+    /**
+     * 要求客户端携带cookies访问
+     */
+    @RequestMapping(value = "/get/with/cookies", method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (Objects.isNull(cookies)) {
+            return "你必须携带cookie信息来访问";
+        }
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("login") &&
+                    cookie.getValue().equals("true")) {
+                return "恭喜你访问成功";
+            }
+        }
+        return "你必须携带cookie信息来访问";
+    }
+
 }
---
+++ document/第10章 项目实战接口开发SpringBoot.md
@@ -0,0 +1,14 @@
+
+## 10-2 springboot简介及官方demo开发
+
+ @Controller
+ @EnableAutoConfiguration
+ 
+ @RequestMapping("/")
+ @ResponseBody
+ 
+ ## 10-3 返回cookies信息的get接口开发
+ @SpringBootApplication
+ @ComponentScan()
+ @RestController
+ 
